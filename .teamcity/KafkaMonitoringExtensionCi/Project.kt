09eb64d3e918e8e9a77464be926afc7299b75d97
VishakaSekar <vishaka.sekar@appdynamics.com>
2019-01-25 10:58:45 -0800

    fixed vcs root

--- .teamcity/KafkaMonitoringExtensionCi/Project.kt
+++ .teamcity/KafkaMonitoringExtensionCi/Project.kt
@@ -14,7 +14,7 @@ object Project : Project({
     parentId("_Root")
     name = "Kafka Monitoring Extension Ci"
 
-    vcsRoot(KafkaMonitoringExtensionCi_HttpsGithubComVishakaSekarKafkaMonitoringExtensionC_2)
+
     vcsRoot(KafkaMonitoringExtensionCi_HttpsGithubComVishakaSekarKafkaMonitoringExtensionCiR)
 
     buildType(KafkaMonitoringExtensionCi_Build)
