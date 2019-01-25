716682d8933aac5c9256d84d18c77f2796ce0de0
admin <admin@TeamCity>
2019-01-25 09:20:56 -0800

    TeamCity change in 'Kafka Monitoring Extension Ci' project: Synchronization with own VCS root is enabled

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,3 @@
-package KafkaMonitoringExtensionCi.buildTypes
-
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCommand
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCompose
@@ -7,12 +5,40 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 
-object KafkaMonitoringExtensionCi_Build : BuildType({
-    uuid = "24861ff0-7d50-4dde-9d1b-b4dcddd74939"
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2018.2"
+
+project {
+
+    buildType(Build)
+}
+
+object Build : BuildType({
     name = "Build"
 
     vcs {
-        root(KafkaMonitoringExtensionCi.vcsRoots.KafkaMonitoringExtensionCi_HttpsGithubComVishakaSekarKafkaMonitoringExtensionCiR)
+        root(DslContext.settingsRoot)
     }
 
     steps {
