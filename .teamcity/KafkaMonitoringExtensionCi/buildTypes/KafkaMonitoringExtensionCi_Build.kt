504f05ef159ccd8f5edbcc5e9aca370296779730
admin <admin@TeamCity>
2019-01-25 09:48:40 -0800

    TeamCity change in 'Kafka Monitoring Extension Ci' project: build configuration was created from URL

--- .teamcity/KafkaMonitoringExtensionCi/buildTypes/KafkaMonitoringExtensionCi_Build.kt
+++ .teamcity/KafkaMonitoringExtensionCi/buildTypes/KafkaMonitoringExtensionCi_Build.kt
@@ -1,40 +1,18 @@
 package KafkaMonitoringExtensionCi.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
-
-
-import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.exec
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
-
 
 object KafkaMonitoringExtensionCi_Build : BuildType({
-    uuid = "8d38ea0b-5554-4d51-9ca2-aa3897610fdb"
+    uuid = "75c498f0-14c7-45a6-a538-53ad0f9b87dc"
     name = "Build"
 
     vcs {
-        root(KafkaMonitoringExtensionCi.vcsRoots.KafkaMonitoringExtensionCi_HttpsGithubComVishakaSekarKafkaMonitoringExtensionCiR)
-    }
-
-    steps {
-        maven {
-            goals = "clean install"
-            mavenVersion = defaultProvidedVersion()
-            jdkHome = "%env.JDK_18%"
-        }
-
-
+        root(KafkaMonitoringExtensionCi.vcsRoots.KafkaMonitoringExtensionCi_HttpsGithubComVishakaSekarKafkaMonitoringExtensionC_2)
     }
 
     triggers {
         vcs {
         }
     }
-
-    artifactRules = """
-       target/KafkaMonitor-*.zip
-    """.trimIndent()
 })
