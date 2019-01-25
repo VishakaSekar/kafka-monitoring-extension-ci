af67efd52fcad609d0de497d75d68e4d7ea268fd
admin <admin@TeamCity>
2019-01-25 08:38:00 -0800

    TeamCity change in 'Kafka Monitoring Extension Ci' project: 9 build step(s) were added to 'Build' build configuration

--- .teamcity/KafkaMonitoringExtensionCi/patches/buildTypes/0c296f0d-4bbb-4819-977c-a59555b0857a.kts
+++ .teamcity/KafkaMonitoringExtensionCi/patches/buildTypes/0c296f0d-4bbb-4819-977c-a59555b0857a.kts
@@ -1,7 +1,11 @@
 package KafkaMonitoringExtensionCi.patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.MavenBuildStep
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCommand
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCompose
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -23,6 +27,57 @@ changeBuildType(uuid("8d38ea0b-5554-4d51-9ca2-aa3897610fdb")) {
         }
     }
     steps {
-        items.removeAt(0)
+        insert(0) {
+            dockerCommand {
+                commandType = build {
+                    source = path {
+                        path = "Dockerfile"
+                    }
+                }
+            }
+        }
+        insert(1) {
+            dockerCompose {
+                file = "docker-compose.yml"
+            }
+        }
+        update<MavenBuildStep>(2) {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            param("teamcity.coverage.idea.includePatterns", "")
+            param("teamcity.coverage.jacoco.patterns", "")
+            param("teamcity.coverage.emma.instr.parameters", "")
+            param("teamcity.coverage.emma.include.source", "")
+        }
+        insert(3) {
+            script {
+                scriptContent = "./broker-list.sh"
+            }
+        }
+        insert(4) {
+            script {
+                scriptContent = "./create-topics.sh"
+            }
+        }
+        insert(5) {
+            script {
+                scriptContent = "./download-kafka.sh"
+            }
+        }
+        insert(6) {
+            script {
+                scriptContent = "./start-kafka-shell.sh"
+            }
+        }
+        insert(7) {
+            script {
+                scriptContent = "./start-kafka.sh"
+            }
+        }
+        insert(8) {
+            script {
+                scriptContent = "./versions.sh"
+            }
+        }
     }
 }
