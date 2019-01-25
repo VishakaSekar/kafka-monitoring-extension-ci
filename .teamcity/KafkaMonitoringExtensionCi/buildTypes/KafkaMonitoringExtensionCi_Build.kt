97014f0c6af3ced79be04c808755712998eb0082
admin <admin@TeamCity>
2019-01-25 09:48:56 -0800

    TeamCity change in 'Kafka Monitoring Extension Ci' project: 9 build step(s) were added to 'Build' build configuration

--- .teamcity/KafkaMonitoringExtensionCi/buildTypes/KafkaMonitoringExtensionCi_Build.kt
+++ .teamcity/KafkaMonitoringExtensionCi/buildTypes/KafkaMonitoringExtensionCi_Build.kt
@@ -1,6 +1,10 @@
 package KafkaMonitoringExtensionCi.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCommand
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCompose
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 
 object KafkaMonitoringExtensionCi_Build : BuildType({
@@ -9,6 +13,43 @@ object KafkaMonitoringExtensionCi_Build : BuildType({
 
     vcs {
         root(KafkaMonitoringExtensionCi.vcsRoots.KafkaMonitoringExtensionCi_HttpsGithubComVishakaSekarKafkaMonitoringExtensionC_2)
+    }
+
+    steps {
+        dockerCompose {
+            file = "docker-compose.yml"
+        }
+        dockerCommand {
+            commandType = build {
+                source = path {
+                    path = "Dockerfile"
+                }
+            }
+        }
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            mavenVersion = defaultProvidedVersion()
+            jdkHome = "%env.JDK_18%"
+        }
+        script {
+            scriptContent = "./broker-list.sh"
+        }
+        script {
+            scriptContent = "./create-topics.sh"
+        }
+        script {
+            scriptContent = "./download-kafka.sh"
+        }
+        script {
+            scriptContent = "./start-kafka-shell.sh"
+        }
+        script {
+            scriptContent = "./start-kafka.sh"
+        }
+        script {
+            scriptContent = "./versions.sh"
+        }
     }
 
     triggers {
