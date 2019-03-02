6528d206fea23e1357d450a2e59820d02cfd365e
vishaka.sekar <vishaka.sekar>
2019-03-02 23:31:36 +0000

    TeamCity change in 'ExtensionsJMX / Kafka Monitoring Extension' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/ExtensionsJMX_KafkaMonitoringExtension/Project.kt
@@ -0,0 +1,29 @@
+package ExtensionsJMX_KafkaMonitoringExtension
+
+import ExtensionsJMX_KafkaMonitoringExtension.vcsRoots.*
+import ExtensionsJMX_KafkaMonitoringExtension.vcsRoots.kafkamonitoringextensionci
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.Project
+import jetbrains.buildServer.configs.kotlin.v2018_2.projectFeatures.VersionedSettings
+import jetbrains.buildServer.configs.kotlin.v2018_2.projectFeatures.versionedSettings
+
+object Project : Project({
+    uuid = "7d4cafb4-fc98-4354-a0ce-366ac8986247"
+    id("ExtensionsJMX_KafkaMonitoringExtension")
+    parentId("ExtensionsJMX")
+    name = "Kafka Monitoring Extension"
+
+    vcsRoot(kafkamonitoringextensionci)
+
+    features {
+        versionedSettings {
+            id = "PROJECT_EXT_24"
+            mode = VersionedSettings.Mode.ENABLED
+            buildSettingsMode = VersionedSettings.BuildSettingsMode.PREFER_SETTINGS_FROM_VCS
+            rootExtId = "${kafkamonitoringextensionci.id}"
+            showChanges = false
+            settingsFormat = VersionedSettings.Format.KOTLIN
+            storeSecureParamsOutsideOfVcs = true
+        }
+    }
+})
---
+++ .teamcity/ExtensionsJMX_KafkaMonitoringExtension/vcsRoots/kafkamonitoringextensionci.kt
@@ -0,0 +1,15 @@
+package ExtensionsJMX_KafkaMonitoringExtension.vcsRoots
+
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
+
+object kafkamonitoringextensionci : GitVcsRoot({
+    uuid = "0727f1a7-b9bf-40fa-a056-787ede9b55ec"
+    name = "kafka"
+    url = "git@github.com:VishakaSekar/kafka-monitoring-extension-ci.git"
+    userNameStyle = GitVcsRoot.UserNameStyle.EMAIL
+    authMethod = uploadedKey {
+        uploadedKey = "v"
+        passphrase = "credentialsJSON:7036c5d7-cf6f-4746-ac0f-681a8777ee4d"
+    }
+})
