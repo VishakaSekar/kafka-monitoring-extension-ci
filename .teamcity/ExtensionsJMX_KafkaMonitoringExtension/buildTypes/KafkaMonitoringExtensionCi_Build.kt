81e2ae77de6d635374e6ad00516ab579c688528a
VishakaSekar <vishaka.sekar@appdynamics.com>
2019-03-02 16:55:43 -0800

    aws .teamcity

---
+++ .teamcity/ExtensionsJMX_KafkaMonitoringExtension/buildTypes/KafkaMonitoringExtensionCi_Build.kt
@@ -0,0 +1,40 @@
+package KafkaMonitoringExtensionCi.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+
+
+import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.exec
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+
+
+object KafkaMonitoringExtensionCi_Build : BuildType({
+    uuid = "8d38ea0b-5554-4d51-9ca2-aa3897610fdb"
+    name = "Build"
+
+    vcs {
+        root(kafkamonitoringextensionci)
+    }
+
+    steps {
+        maven {
+            goals = "clean install"
+            mavenVersion = defaultProvidedVersion()
+            jdkHome = "%env.JDK_18%"
+        }
+
+
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    artifactRules = """
+       target/KafkaMonitor-*.zip
+    """.trimIndent()
+})
